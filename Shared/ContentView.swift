c23766a69d129232db9216991d7178af2372da01
WataruSuzuki <wataru0406@hotmail.co.jp>
2021-07-28 22:10:00 +0900

    Created xcode project

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Wataru Suzuki on 2021/07/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/HelloCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCombineApp.swift
+//  Shared
+//
+//  Created by Wataru Suzuki on 2021/07/28.
+//
+
+import SwiftUI
+
+@main
+struct HelloCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
